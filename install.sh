603d84ae0cee4fc190e1d33cd0932f27f3600879
Brian Mayo <ioprotium@gmail.com>
2021-12-13 17:48:33 +0100

    refactor: group config commands

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 . scripts/utils.sh
-. brew_utils/utils.sh
+. scripts/brew_utils.sh
 . scripts/apps.sh
 . scripts/cli.sh
 . scripts/config.sh
@@ -38,22 +38,22 @@ function main() {
   wait_input
 
   install_oh_my_zsh
-  chsh -s /bin/zsh
   print_green "Finished installing Oh-my-zsh"
   wait_input
 
   install_macos_apps
-  print_green "Finished macOS apps"
+  print_green "Finished installing macOS apps"
   wait_input
 
+  install_neovim
+  print_green "Finished installing neovim"
+  wait_input
+
+  print_blue "Starting configuration"
   configure_macos_defaults
   print_green "Finished configuring defaults. NOTE: A restart is needed"
-  wait_input
-
   code_as_default_text_editor
   print_green "Finished setting up vscode as default text editor"
-  wait_input
-
   configure_iterm
   print_green "Finished setting up vscode as default text editor"
   stow_dotfiles
@@ -61,7 +61,7 @@ function main() {
   print_blue "Install NeoVim plugins"
   nvim -c ':PlugInstall' -c ':UpdateRemotePlugins' -c ':qall'
 
-  print_green "Installation finished"
+  print_green "Done"
 }
 
 main
