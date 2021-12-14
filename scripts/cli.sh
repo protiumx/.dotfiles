2e374c68e7d92b4373b9d1a6b9ac8d107c695773
Brian Mayo <ioprotium@gmail.com>
2021-12-14 23:55:14 +0100

    do not change shell

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -7,27 +7,12 @@ function install_xcode_clt() {
   fi
 }
 
-function install_homebrew() {
-  export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-  if hash brew &>/dev/null; then
-    print_yellow "Homebrew already installed. Getting updates and package upgrades..."
-    brew update
-    brew upgrade
-    brew doctor
-  else
-    print_blue "Installing homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    brew update
-  fi  
-}
-
 function install_oh_my_zsh() {
   if [[ ! -f ~/.zshrc ]]; then
     print_blue "Installing oh my zsh..."
     ZSH=~/.oh-my-zsh ZSH_DISABLE_COMPFIX=true sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     chmod 744 ~/.oh-my-zsh/oh-my-zsh.sh
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-    chsh -s /bin/zsh
   else
     print_green "oh my zsh already installed"
   fi
