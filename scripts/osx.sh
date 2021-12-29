c99169cb37152075f8f0e6d24348e2cd6f05f738
Brian Mayo <ioprotium@gmail.com>
2021-12-29 12:40:22 +0100

    added more osx settings

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -1,19 +1,52 @@
 setup_osx() {
   info "Configuring MacOS default settings"
 
-  # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-  defaults write com.apple.screencapture type -string "png"
+  # Disable prompting to use new exteral drives as Time Machine volume
+  defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-  # Set Desktop as the default location for new Finder windows
-  # For other paths, use `PfLo` and `file:///full/path/here/`
-  defaults write com.apple.finder NewWindowTarget -string "PfDe"
-  defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+  # Hide external hard drives on desktop
+  defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+
+  # Hide hard drives on desktop
+  defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+
+  # Hide removable media hard drives on desktop
+  defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+
+  # Hide mounted servers on desktop
+  defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+
+  # Hide icons on desktop
+  defaults write com.apple.finder CreateDesktop -bool false
 
   # When performing a search, search the current folder by default
   defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
   # Avoid creating .DS_Store files on network volumes
   defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+  # Show path bar
+  defaults write com.apple.finder ShowPathbar -bool true
+
+  # Show hidden files inside the finder
+  defaults write com.apple.finder "AppleShowAllFiles" -bool true
+
+  # Do not show warning when changing the file extension
+  defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+  # Set search scope to current folder
+  defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+  # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+  defaults write com.apple.screencapture type -string "png"
+
+  # Set weekly software update checks
+  defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 7
+
+  # Set Desktop as the default location for new Finder windows
+  # For other paths, use `PfLo` and `file:///full/path/here/`
+  defaults write com.apple.finder NewWindowTarget -string "PfDe"
+  defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 
   # Enable the Develop menu and the Web Inspector in Safari
   defaults write com.apple.Safari IncludeDevelopMenu -bool true
@@ -22,15 +55,6 @@ setup_osx() {
 
   # Add a context menu item for showing the Web Inspector in web views
   defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-  # Show hidden files inside the finder
-  defaults write com.apple.Finder "AppleShowAllFiles" -bool true
-
-  # Do not show warning when changing the file extension
-  defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-  # Show path bar
-  defaults write com.apple.finder ShowPathbar -bool true
 
   # Have the Dock show only active apps
   defaults write com.apple.dock static-only -bool true
