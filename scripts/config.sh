36049a1f5e59a8926c7e711a3c2158b9b1f16702
Brian Mayo <ioprotium@gmail.com>
2022-11-22 23:18:22 +0100

    stow wezterm

--- scripts/config.sh
+++ scripts/config.sh
@@ -61,6 +61,7 @@ stow_dotfiles() {
     ".config/nvim"
     ".config/ripgrep"
     ".config/vim"
+    ".config/wezterm"
     ".git-templates/hooks"
     ".ssh"
   )
---
+++ stow/wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,257 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+local mux = wezterm.mux
+
+local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    wezterm.log_info(tab.active_pane)
+    return {
+      { Text = wezterm.nerdfonts.fa_chevron_right .. ' ' .. tab.active_pane.title .. ' ' },
+    }
+  end
+)
+
+return {
+  audible_bell = "Disabled",
+  color_scheme = "PaperColorDark",
+  font = wezterm.font("FiraCode Nerd Font", {weight="Medium", stretch="Normal", style="Normal"}),
+  font_rules = {
+    {
+      intensity = 'Bold',
+      font = wezterm.font("FiraCode Nerd Font", {weight="Bold", stretch="Normal", style="Normal"}),
+    },
+  },
+  font_size = 19.0,
+  hide_tab_bar_if_only_one_tab = true,
+  inactive_pane_hsb = {
+    brightness = 0.8,
+  },
+
+  keys = {
+    { key = 'p', mods = 'CMD', action = wezterm.action.ShowLauncher },
+    { key = 'd', mods = 'ALT', action = wezterm.action.ShowDebugOverlay },
+
+    -- Panes
+    {
+      key = 'd',
+      mods = 'CMD',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = 'D',
+      mods = 'SHIFT|CMD',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = 'w',
+      mods = 'CMD',
+      action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+
+    -- Activation
+    {
+      key = 'LeftArrow',
+      mods = 'CMD',
+      action = act.ActivatePaneDirection 'Left',
+    },
+    {
+      key = 'RightArrow',
+      mods = 'CMD',
+      action = act.ActivatePaneDirection 'Right',
+    },
+    {
+      key = 'UpArrow',
+      mods = 'CMD',
+      action = act.ActivatePaneDirection 'Up',
+    },
+    {
+      key = 'DownArrow',
+      mods = 'CMD',
+      action = act.ActivatePaneDirection 'Down',
+    },
+
+    -- Size
+    {
+      key = 'H',
+      mods = 'SHIFT|CMD',
+      action = act.AdjustPaneSize { 'Left', 1 },
+    },
+    {
+      key = 'J',
+      mods = 'SHIFT|CMD',
+      action = act.AdjustPaneSize { 'Down', 1 },
+    },
+    { 
+      key = 'K',
+      mods = 'SHIFT|CMD',
+      action = act.AdjustPaneSize { 'Up', 1 } },
+    {
+      key = 'L',
+      mods = 'SHIFT|CMD',
+      action = act.AdjustPaneSize { 'Right', 1 },
+    },
+
+    -- Rotate
+    {
+      key = 'r',
+      mods = 'CMD',
+      action = act.RotatePanes 'CounterClockwise',
+    },
+    { 
+      key = 'R',
+      mods = 'SHIFT|CMD', 
+      action = act.RotatePanes 'Clockwise',
+    },
+
+    { 
+      key = 'S', 
+      mods = 'SHIFT|CTRL', 
+      action = act.PaneSelect,
+    },
+
+    {
+      key = 'S',
+      mods = 'SHIFT|CMD',
+      action = act.PaneSelect {
+        mode = 'SwapWithActive',
+      },
+    },
+
+
+    -- Tabs
+    { key = ']', mods = 'CMD', action = wezterm.action.ShowTabNavigator },
+    { key = 'Tab', mods = 'SHIFT|CTRL', action = act.ActivateTabRelative(-1) },
+    { key = 'Tab', mods = 'CTRL', action = act.ActivateTabRelative(1) },
+
+    {
+      key = 'k',
+      mods = 'CTRL',
+      action = act.Multiple {
+        act.ClearScrollback 'ScrollbackAndViewport',
+        act.SendKey { key = 'L', mods = 'CTRL' },
+      },
+    },
+
+    {
+      key = '0',
+      mods = 'CTRL',
+      action = wezterm.action.ResetFontAndWindowSize,
+    },
+
+    -- Utils
+    {
+      key = 'V',
+      mods = 'SHIFT|CMD',
+      action = wezterm.action.SplitPane {
+        direction = 'Right',
+        size = { Percent = 35 },
+      },
+    },
+
+    {
+      key = 'LeftArrow',
+      mods = 'OPT',
+      action = act.SendKey {
+        key = 'b',
+        mods = 'ALT',
+      },
+    },
+
+    {
+      key = 'RightArrow',
+      mods = 'OPT',
+      action = act.SendKey { key = 'f', mods = 'ALT' },
+    },
+
+    {
+      key = 'LeftArrow',
+      mods = 'CMD',
+      action = act.SendKey {
+        key = 'a',
+        mods = 'CTRL',
+      },
+    },
+
+    {
+      key = 'RightArrow',
+      mods = 'CMD',
+      action = act.SendKey { key = 'e', mods = 'CTRL' },
+    },
+  },
+  scrollback_lines = 6000,
+  send_composed_key_when_left_alt_is_pressed = false,
+  show_new_tab_button_in_tab_bar = false,
+  switch_to_last_active_tab_when_closing_tab = true,
+
+  window_decorations = "RESIZE",
+  window_frame = {
+    -- The font used in the tab bar.
+    -- Roboto Bold is the default; this font is bundled
+    -- with wezterm.
+    -- Whatever font is selected here, it will have the
+    -- main font setting appended to it to pick up any
+    -- fallback fonts you may have used there.
+    font = wezterm.font { family = 'Fira Code', weight = 'Bold' },
+
+    -- The size of the font in the tab bar.
+    -- Default to 10. on Windows but 12.0 on other systems
+    font_size = 14.0,
+
+    -- The overall background color of the tab bar when
+    -- the window is focused
+    active_titlebar_bg = '#1c1c1c',
+
+    -- The overall background color of the tab bar when
+    -- the window is not focused
+    inactive_titlebar_bg = '#1c1c1c',
+  },
+
+  cursor_blink_rate = 400,
+  default_cursor_style = "BlinkingBar",
+  cursor_thickness = 1.5,
+
+  colors = {
+    background = "#1c1c1c",
+    cursor_bg = '#ff5faf',
+    selection_fg = '#1c1c1c',
+    selection_bg = '#ff5faf',
+    tab_bar = {
+      -- The color of the strip that goes along the top of the window
+      -- (does not apply when fancy tab bar is in use)
+      background = '#1c1c1c',
+      inactive_tab_edge = '#1c1c1c',
+
+      active_tab = {
+        bg_color = '#1c1c1c',
+        fg_color = '#c0c0c0',
+      },
+
+      -- Inactive tabs are the tabs that do not have focus
+      inactive_tab = {
+        bg_color = '#1c1c1c',
+        fg_color = '#808080',
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `inactive_tab`.
+      },
+
+      -- You can configure some alternate styling when the mouse pointer
+      -- moves over inactive tabs
+      inactive_tab_hover = {
+        bg_color = '#1c1c1c',
+        fg_color = '#808080',
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `inactive_tab_hover`.
+      },
+    },
+  },
+}
