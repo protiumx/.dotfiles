efbfc7cb421908bfed9ab2357df85bc98c97b863
Brian Mayo <ioprotium@gmail.com>
2022-12-21 19:45:38 +0100

    treesitter config

---
+++ stow/nvim/.config/nvim/after/plugin/treesitter.lua
@@ -0,0 +1,15 @@
+require('nvim-treesitter.configs').setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "help", "javascript", "go", "typescript", "c", "lua", "rust" },
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
+
