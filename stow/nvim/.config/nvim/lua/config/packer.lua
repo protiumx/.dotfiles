1d051166a3d7b3f757e4ca054fcae7efff3d7770
Brian Mayo <ioprotium@gmail.com>
2022-12-23 05:58:39 +0100

    install packer from lua

--- stow/nvim/.config/nvim/lua/config/packer.lua
+++ stow/nvim/.config/nvim/lua/config/packer.lua
@@ -1,3 +1,25 @@
+-- Automatically install packer
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+  return
+end
+
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
@@ -88,4 +110,9 @@ return require('packer').startup(function(use)
     "glepnir/lspsaga.nvim",
     branch = "main",
   })
+
+  -- Sync plugins after installing packer
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
