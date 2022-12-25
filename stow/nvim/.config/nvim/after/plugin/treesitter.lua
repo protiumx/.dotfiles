b518e04ecb91ba20863a7c648932a23b4242a505
Brian Mayo <ioprotium@gmail.com>
2022-12-25 04:04:50 +0100

    disable treesitter autoinstall

--- stow/nvim/.config/nvim/after/plugin/treesitter.lua
+++ stow/nvim/.config/nvim/after/plugin/treesitter.lua
@@ -11,15 +11,12 @@ require('nvim-treesitter.configs').setup {
     'rust',
   },
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+  auto_install = false,
 
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
-    disable = {'go'},
+    -- PaperColor looks way better
+    disable = { 'go' },
   },
 }
-
