8c78bb46e91b391d87d93817fdc7ea6d819d38bc
Brian Mayo <ioprotium@gmail.com>
2022-12-25 04:00:53 +0100

    format

--- stow/nvim/.config/nvim/after/plugin/feline.lua
+++ stow/nvim/.config/nvim/after/plugin/feline.lua
@@ -17,111 +17,111 @@ local colors = {
 }
 
 local modes = {
-	["__"] = "--",
-	["c"] = "C",
-	["i"] = "I",
-	["ic"] = "I-C",
-	["ix"] = "I-X",
-	["multi"] = "M",
-	["n"] = "N",
-	["ni"] = "(I)",
-	["no"] = "OP",
-	["R"] = "R",
-	["Rv"] = "V-R",
-	["s"] = "S",
-	["S"] = "S-L",
-	[""] = "S-B",
-	["t"] = "T",
-	["v"] = "V",
-	["V"] = "V-L",
-	[""] = "V-B",
+  ["__"] = "--",
+  ["c"] = "C",
+  ["i"] = "I",
+  ["ic"] = "I-C",
+  ["ix"] = "I-X",
+  ["multi"] = "M",
+  ["n"] = "N",
+  ["ni"] = "(I)",
+  ["no"] = "OP",
+  ["R"] = "R",
+  ["Rv"] = "V-R",
+  ["s"] = "S",
+  ["S"] = "S-L",
+  [""] = "S-B",
+  ["t"] = "T",
+  ["v"] = "V",
+  ["V"] = "V-L",
+  [""] = "V-B",
 }
 
 local mode_colors = {
-	n = colors.blue,
-	i = colors.yellow,
-	v = colors.magenta,
-	[""] = colors.lightblue,
-	[""] = colors.white,
-	V = colors.lightblue,
-	c = colors.orange,
-	no = colors.magenta,
-	s = colors.orange,
-	S = colors.orange,
-	-- [""] = colors.orange,
-	ic = colors.yellow,
-	R = colors.purple,
-	Rv = colors.purple,
-	cv = colors.orange,
-	ce = colors.orange,
-	r = colors.cyan,
-	rm = colors.cyan,
-	["r?"] = colors.cyan,
-	["!"] = colors.cyan,
-	t = colors.cyan,
+  n = colors.blue,
+  i = colors.yellow,
+  v = colors.magenta,
+  [""] = colors.lightblue,
+  [""] = colors.white,
+  V = colors.lightblue,
+  c = colors.orange,
+  no = colors.magenta,
+  s = colors.orange,
+  S = colors.orange,
+  -- [""] = colors.orange,
+  ic = colors.yellow,
+  R = colors.purple,
+  Rv = colors.purple,
+  cv = colors.orange,
+  ce = colors.orange,
+  r = colors.cyan,
+  rm = colors.cyan,
+  ["r?"] = colors.cyan,
+  ["!"] = colors.cyan,
+  t = colors.cyan,
 }
 
 local comps = {
-    vi_mode = {
-      provider = function()
-        return '| ' .. modes[vim.fn.mode()] .. '  '
-      end,
-      hl = function()
-          local val = {
-              fg = mode_colors[vim.fn.mode()],
-              bg = "#2c2c2c",
-              style = 'bold'
-          }
-          return val
-      end,
-      right_sep = '  '
+  vi_mode = {
+    provider = function()
+      return '| ' .. modes[vim.fn.mode()] .. '  '
+    end,
+    hl = function()
+      local val = {
+        fg = mode_colors[vim.fn.mode()],
+        bg = "#2c2c2c",
+        style = 'bold'
+      }
+      return val
+    end,
+    right_sep = '  '
+  },
+
+  file = {
+    info = {
+      provider = {
+        name = 'file_info',
+        opts = {
+          type = 'relative',
+          file_readonly_icon = '  ',
+          file_modified_icon = ' * ',
+        }
+      },
+      icon = '',
+      right_sep = '',
+      hl = {
+        -- bg = '#2c2c2c',
+        fg = colors.blue,
+      }
     },
 
-    file = {
-      info = {
-          provider = {
-            name = 'file_info',
-            opts = {
-              type = 'relative',
-              file_readonly_icon = '  ',
-              file_modified_icon = ' * ',
-            }
-          },
-          icon = '',
-          right_sep = '',
-          hl = {
-              -- bg = '#2c2c2c',
-              fg = colors.blue,
-          }
-      },
-
-      encoding = {
-          provider = 'file_encoding',
-          left_sep = ' ',
-          hl = {
-              fg = colors.violet,
-          }
-      },
-
-      position = {
-          provider = {
-            name = "position",
-            opts = {
-              format = "{line}:{col}",
-            },
-          },
-          left_sep = ' ',
-          hl = {
-              fg = colors.cyan,
-          }
-      },
-    },
-
-    line_percentage = {
-      provider = 'line_percentage',
+    encoding = {
+      provider = 'file_encoding',
       left_sep = ' ',
-      hl = {},
+      hl = {
+        fg = colors.violet,
+      }
     },
+
+    position = {
+      provider = {
+        name = "position",
+        opts = {
+          format = "{line}:{col}",
+        },
+      },
+      left_sep = ' ',
+      hl = {
+        fg = colors.cyan,
+      }
+    },
+  },
+
+  line_percentage = {
+    provider = 'line_percentage',
+    left_sep = ' ',
+    hl = {},
+  },
 }
 
 local components = {
@@ -148,21 +148,20 @@ table.insert(components.inactive[1], comps.file.info)
 
 local feline = require('feline')
 feline.setup({
-    colors = colors,
-    components = components,
-    vi_mode_colors = mode_colors,
-    disable = {
-        filetypes = {
-          '^NvimTree$',
-          '^packer$',
-          '^startify$',
-          '^fugitive$',
-          '^fugitiveblame$',
-          '^qf$',
-          '^help$'
-        },
-        buftypes = {'^terminal$'},
-        bufnames = {}
-    }
+  colors = colors,
+  components = components,
+  vi_mode_colors = mode_colors,
+  disable = {
+    filetypes = {
+      '^packer$',
+      '^fugitive$',
+      '^fugitiveblame$',
+      '^qf$',
+      '^help$'
+    },
+    buftypes = { '^terminal$' },
+    bufnames = {}
+  }
 })
+
 feline.use_theme(colors)
