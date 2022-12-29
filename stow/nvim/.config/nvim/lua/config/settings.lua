0e4cd283a247c1e5c9c76ff8d906f592dfee670e
Brian Mayo <ioprotium@gmail.com>
2022-12-29 23:29:34 +0100

    disable hl

--- stow/nvim/.config/nvim/lua/config/settings.lua
+++ stow/nvim/.config/nvim/lua/config/settings.lua
@@ -30,7 +30,7 @@ vim.o.completeopt = 'menuone,noselect'
 vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
 
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
