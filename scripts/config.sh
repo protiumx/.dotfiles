30910ff11733a3857a3495dcada8345c6167588a
Brian Mayo <ioprotium@gmail.com>
2023-01-04 22:42:54 +0100

    check ssh phrase is set

--- scripts/config.sh
+++ scripts/config.sh
@@ -34,13 +34,17 @@ code_as_default_text_editor() {
 }
 
 setup_github_ssh() {
-  info "Using $SSH_PASSPHRASE"
-  ssh-keygen -t ed25519 -C "$SSH_PASSPHRASE"
+  if [ -z "${SSH_PASSPHRASE}" ]; then
+    echo "SSH_PASSPHRASE not set"
+  else
+    info "Using $SSH_PASSPHRASE"
+    ssh-keygen -t ed25519 -C "$SSH_PASSPHRASE"
 
-  info "Adding ssh key to keychain"
-  ssh-add -K ~/.ssh/id_ed25519
+    info "Adding ssh key to keychain"
+    ssh-add -K ~/.ssh/id_ed25519
 
-  info "Remember add ssh key to github account 'pbcopy < ~/.ssh/id_ed25519.pub'"
+    info "Remember add ssh key to github account 'pbcopy < ~/.ssh/id_ed25519.pub'"
+  fi
 }
 
 stow_dotfiles() {
