cfc1651bb12e6abe33003e97a14c3e752ce85b90
protiumx <ioprotium@gmail.com>
2023-01-23 20:51:22 +0100

    added bash

--- stow/nvim/.config/nvim/lua/config/treesitter.lua
+++ stow/nvim/.config/nvim/lua/config/treesitter.lua
@@ -62,6 +62,7 @@ function M.setup()
   require('nvim-treesitter.configs').setup({
     -- A list of parser names, or "all"
     ensure_installed = {
+      'bash',
       'help',
       'javascript',
       'go',
