2bd7d241148dfa4252d00a985bf7e89063bb12f0
protiumx <ioprotium@gmail.com>
2023-01-27 17:46:57 +0100

    filter whitespaces out

--- stow/nvim/.config/nvim/lua/config/neoclip.lua
+++ stow/nvim/.config/nvim/lua/config/neoclip.lua
@@ -1,6 +1,19 @@
 local M = {}
 
 function M.setup()
+  local function is_whitespace(line)
+    return vim.fn.match(line, [[^\s*$]]) ~= -1
+  end
+
+  local function all(tbl, check)
+    for _, entry in ipairs(tbl) do
+      if not check(entry) then
+        return false
+      end
+    end
+    return true
+  end
+
   require('neoclip').setup({
     history = 20,
     enable_persistent_history = false,
@@ -11,11 +24,16 @@ function M.setup()
     default_register_macros = 'q',
     enable_macro_history = true,
     content_spec_column = false,
+    filter = function(data)
+      return not all(data.event.regcontents, is_whitespace)
+    end,
     on_paste = {
-      set_reg = false,
+      set_reg = true,
+      move_to_front = false,
     },
     on_replay = {
       set_reg = false,
+      move_to_front = false,
     },
     keys = {
       telescope = {
