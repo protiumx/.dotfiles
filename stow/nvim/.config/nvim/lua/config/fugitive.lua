5fabef0c67e2b75ad5134db2d8c23074e4c329e6
Brian Mayo <ioprotium@gmail.com>
2023-01-27 21:13:47 +0000

    map commands to "g"

--- stow/nvim/.config/nvim/lua/config/fugitive.lua
+++ stow/nvim/.config/nvim/lua/config/fugitive.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 function M.setup()
-  vim.keymap.set('n', '<Leader>Gc', ':G commit<CR>', { silent = true })
-  vim.keymap.set('n', '<Leader>Gs', ':G stage %<CR>', { silent = true })
+  vim.keymap.set('n', '<Leader>gc', ':G commit<CR>', { silent = true })
+  vim.keymap.set('n', '<Leader>gs', ':G stage %<CR>', { silent = true })
 end
 
 return M
