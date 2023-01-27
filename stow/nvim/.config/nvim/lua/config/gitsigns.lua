f3493fe7968dd478f826b09ccb5ac0f9b66bdd03
Brian Mayo <ioprotium@gmail.com>
2023-01-27 21:12:28 +0000

    nice git mappings

--- stow/nvim/.config/nvim/lua/config/gitsigns.lua
+++ stow/nvim/.config/nvim/lua/config/gitsigns.lua
@@ -3,37 +3,42 @@ local M = {}
 local function on_attach(bufnr)
   local gs = package.loaded.gitsigns
 
-  local function map(mode, l, r, opts)
-    opts = opts or {}
-    opts.buffer = bufnr
+  local function map(mode, l, r, desc)
+    local opts = { silent = true, desc = desc, bufnr = bufnr }
     vim.keymap.set(mode, l, r, opts)
   end
 
-  -- Navigation
-  map('n', ']c', function()
+  -- Hunk Navigation
+  vim.keymap.set('n', ']c', function()
     if vim.wo.diff then return ']c' end
     vim.schedule(function() gs.next_hunk() end)
     return '<Ignore>'
   end, { expr = true })
 
-  map('n', '[c', function()
+  vim.keymap.set('n', '[c', function()
     if vim.wo.diff then return '[c' end
     vim.schedule(function() gs.prev_hunk() end)
     return '<Ignore>'
   end, { expr = true })
 
   -- Actions
-  map('n', '<Leader>gp', gs.preview_hunk)
-  map('n', '<Leader>gb', gs.blame_line)
-  map('n', '<Leader>gB', function() gs.blame_line({ full = true }) end)
-  map('n', '<Leader>glb', gs.toggle_current_line_blame)
-  map('n', '<Leader>gd', gs.diffthis)
+  map('n', '<Leader>gp', gs.preview_hunk, '[Git] Preview hunk')
+  map('n', '<Leader>gb', gs.blame_line, '[Git] Blame line short')
+  map('n', '<Leader>gB', function()
+    gs.blame_line({ full = true })
+  end, '[Git] Blame line full')
+
+  map('n', '<Leader>glb', gs.toggle_current_line_blame, '[Git] Toggle line blame')
+  map('n', '<Leader>gd', gs.diffthis, '[Git] Show diff of current file')
   -- TODO: make it usable
-  map('n', '<Leader>gD', function() gs.diffthis('~') end)
-  map('n', '<Leader>gtd', gs.toggle_deleted)
+  map('n', '<Leader>gD', function()
+    gs.diffthis('~')
+  end)
+
+  map('n', '<Leader>gtd', gs.toggle_deleted, '[Git] Toggle deleted preview')
 
   -- Text object
-  map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', '[Git] Hunk Text Object')
 end
 
 function M.setup()
