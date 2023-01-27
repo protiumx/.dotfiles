019be9d53b6a2419d9746d766a65d6276bc76e21
protiumx <ioprotium@gmail.com>
2023-01-27 17:29:53 +0100

    added commit and stage map

--- stow/nvim/.config/nvim/lua/config/fugitive.lua
+++ stow/nvim/.config/nvim/lua/config/fugitive.lua
@@ -1,16 +1,8 @@
 local M = {}
 
 function M.setup()
-  vim.keymap.set('n', '<F3>', function()
-    local name = vim.fn.bufname('fugitive:///*/.git//')
-    if name ~= '' and vim.fn.buflisted(name) ~= 0 then
-      vim.cmd [[ execute ":bd" bufname('fugitive:///*/.git//') ]]
-    else
-      vim.cmd [[vertical Git | vertical resize 40 | setlocal noequalalways wrap readonly nomodifiable noswapfile]]
-    end
-  end)
-
-  vim.keymap.set('n', '<Leader>Go', ':G | only<CR>', { silent = true })
+  vim.keymap.set('n', '<Leader>Gc', ':G commit', { silent = true })
+  vim.keymap.set('n', '<Leader>Gs', ':G stage %', { silent = true })
 end
 
 return M
