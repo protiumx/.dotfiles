a2309ba6d08f41b5b055b991a96a362c56419758
protiumx <ioprotium@gmail.com>
2023-01-27 19:29:17 +0100

    maps to stage and commit

--- stow/nvim/.config/nvim/lua/config/fugitive.lua
+++ stow/nvim/.config/nvim/lua/config/fugitive.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 function M.setup()
-  vim.keymap.set('n', '<Leader>Gc', ':G commit', { silent = true })
-  vim.keymap.set('n', '<Leader>Gs', ':G stage %', { silent = true })
+  vim.keymap.set('n', '<Leader>Gc', ':G commit<CR>', { silent = true })
+  vim.keymap.set('n', '<Leader>Gs', ':G stage %<CR>', { silent = true })
 end
 
 return M
