79e33eb3f70f7dfaf03aedb3bc23ee240b57a2d5
Brian Mayo <ioprotium@gmail.com>
2023-01-28 01:55:01 +0000

    more keymaps

--- stow/nvim/.config/nvim/lua/config/diffview.lua
+++ stow/nvim/.config/nvim/lua/config/diffview.lua
@@ -26,9 +26,21 @@ function M.setup()
     },
 
     keymaps = {
+      view = {
+        { 'n', '<C-o>', actions.goto_file_tab, { desc = 'Open the file in a new tabpage' } },
+        { 'n', '<Leader>P', actions.focus_files, { desc = 'Bring focus to the file panel' } },
+        { 'n', '<Leader>p', actions.toggle_files, { desc = 'Toggle the file panel.' } },
+      },
+
       file_panel = {
         { 'n', '<C-o>', actions.goto_file_tab, { desc = 'Open the file in a new tabpage' } },
-        { 'n', 'gf', actions.goto_file, { desc = 'Open the file in a new split in the previous tabpage' } },
+        { 'n', '<Leader>P', actions.focus_files, { desc = 'Bring focus to the file panel' } },
+        { 'n', '<Leader>p', actions.toggle_files, { desc = 'Toggle the file panel' } },
+        { 'n', 'q', '<cmd>DiffviewClose<CR>', { silent = true } },
+      },
+
+      file_history_panel = {
+        { 'n', '<C-o>', actions.goto_file_tab, { desc = 'Open the file in a new tabpage' } },
         { 'n', '<Leader>P', actions.focus_files, { desc = 'Bring focus to the file panel' } },
         { 'n', '<Leader>p', actions.toggle_files, { desc = 'Toggle the file panel' } },
         { 'n', 'q', '<cmd>DiffviewClose<CR>', { silent = true } },
