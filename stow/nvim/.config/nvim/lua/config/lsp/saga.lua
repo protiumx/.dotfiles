da83bd07f0ac60ba05d71775778dcc81f78e73de
Brian Mayo <ioprotium@gmail.com>
2023-01-28 18:21:24 +0000

    better keymaps

--- stow/nvim/.config/nvim/lua/config/lsp/saga.lua
+++ stow/nvim/.config/nvim/lua/config/lsp/saga.lua
@@ -3,11 +3,12 @@ local M = {}
 local function keymaps()
   local keymap = vim.keymap.set
   keymap('n', 'gh', '<cmd>Lspsaga lsp_finder<CR>', { silent = true })
-  keymap('i', '<C-h>', '<Esc><cmd>Lspsaga lsp_finder<CR>', { silent = true })
-  keymap({ 'n', 'v' }, '<Leader>Ca', '<cmd>Lspsaga code_action<CR>', { silent = true })
+  keymap('i', '<M-h>', '<Esc><cmd>Lspsaga lsp_finder<CR>', { silent = true })
+  keymap({ 'n', 'v' }, '<Leader>ca', '<cmd>Lspsaga code_action<CR>', { silent = true })
   keymap('n', '<Leader>rn', '<cmd>Lspsaga rename<CR>', { silent = true })
   keymap('n', '<F2>', '<cmd>Lspsaga rename<CR>', { silent = true })
   keymap('n', 'gpd', '<cmd>Lspsaga peek_definition<CR>', { silent = true })
+  keymap('n', 'gD', '<cmd>Lspsaga hover_doc<CR>', { silent = true })
 
   -- Only jump to errors
   keymap('n', '[E', function()
