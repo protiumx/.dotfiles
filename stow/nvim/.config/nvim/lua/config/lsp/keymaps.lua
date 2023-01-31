214b86b28a2d6a105fe2a6d46d0706e58da5fe22
protiumx <ioprotium@gmail.com>
2023-01-31 20:27:13 +0100

    better mappings

--- stow/nvim/.config/nvim/lua/config/lsp/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/lsp/keymaps.lua
@@ -9,7 +9,6 @@ function M.setup(bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('gd', vim.lsp.buf.definition, 'go to definitions')
   nmap('gy', vim.lsp.buf.type_definition, 'show type definition')
   nmap('gi', vim.lsp.buf.implementation, 'go to implementation')
   nmap('[d', vim.diagnostic.goto_next, 'go to next diagnostic symbols')
