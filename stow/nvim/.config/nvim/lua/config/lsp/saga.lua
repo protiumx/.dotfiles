966424e069e1969802b279a1acf146ad5c8e793d
protiumx <ioprotium@gmail.com>
2023-01-31 20:26:24 +0100

    better mappings

--- stow/nvim/.config/nvim/lua/config/lsp/saga.lua
+++ stow/nvim/.config/nvim/lua/config/lsp/saga.lua
@@ -4,11 +4,24 @@ local function keymaps()
   local keymap = vim.keymap.set
   keymap('n', 'gh', '<cmd>Lspsaga lsp_finder<CR>', { silent = true })
   keymap('i', '<M-h>', '<Esc><cmd>Lspsaga lsp_finder<CR>', { silent = true })
+
   keymap({ 'n', 'v' }, '<Leader>ca', '<cmd>Lspsaga code_action<CR>', { silent = true })
-  keymap('n', '<Leader>rn', '<cmd>Lspsaga rename<CR>', { silent = true })
+  keymap('n', 'gr', '<cmd>Lspsaga rename<CR>', { silent = true })
   keymap('n', '<F2>', '<cmd>Lspsaga rename<CR>', { silent = true })
-  keymap('n', 'gpd', '<cmd>Lspsaga peek_definition<CR>', { silent = true })
-  keymap('n', 'gD', '<cmd>Lspsaga hover_doc<CR>', { silent = true })
+
+  keymap('n', '<M-d>', '<cmd>Lspsaga peek_definition<CR>', { silent = true })
+  -- Go to definition
+  keymap('n', 'gd', '<cmd>Lspsaga goto_definition<CR>')
+
+  -- Show buffer diagnostics
+  keymap('n', '<Leader>ld', '<cmd>Lspsaga show_buf_diagnostics<CR>')
+
+  keymap('n', 'K', '<cmd>Lspsaga hover_doc<CR>', { silent = true })
+
+  -- Diagnostic jump
+  -- You can use <C-o> to jump back to your previous location
+  keymap('n', '[e', '<cmd>Lspsaga diagnostic_jump_prev<CR>')
+  keymap('n', ']e', '<cmd>Lspsaga diagnostic_jump_next<CR>')
 
   -- Only jump to errors
   keymap('n', '[E', function()
@@ -19,7 +32,7 @@ local function keymaps()
   end, { silent = true })
 
   -- Outline
-  keymap('n', '<Leader>So', '<cmd>Lspsaga outline<CR>', { silent = true })
+  keymap('n', '<M-S-H>', '<cmd>Lspsaga outline<CR>', { silent = true })
 end
 
 function M.setup()
