c88465ea04e28ea5d3026e765e902064769a1e97
protiumx <ioprotium@gmail.com>
2023-02-01 13:27:15 +0100

    use accent color for highlights

--- stow/nvim/.config/nvim/lua/config/vim-sneak.lua
+++ stow/nvim/.config/nvim/lua/config/vim-sneak.lua
@@ -1,12 +1,18 @@
 local M = {}
 
 function M.setup()
+  local colors = require('config.colors')
+
   vim.keymap.set('', 's', '<Plug>Sneak_s', { remap = true })
   vim.keymap.set('', 'S', '<Plug>Sneak_S', { remap = true })
   vim.keymap.set('', 'f', '<Plug>Sneak_f', { remap = true })
   vim.keymap.set('', 'F', '<Plug>Sneak_F', { remap = true })
   vim.keymap.set('', 't', '<Plug>Sneak_t', { remap = true })
   vim.keymap.set('', 'T', '<Plug>Sneak_T', { remap = true })
+
+  for _, v in ipairs({ 'Sneak', 'SneakLabel', 'SneakLabelMask', 'SneakScope' }) do
+    vim.api.nvim_set_hl(0, v, { bg = colors.accent, fg = colors.background })
+  end
 end
 
 return M
