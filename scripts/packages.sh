241a3cdfff0f78f56a5b262e27607d4bbea90561
Brian Mayo <ioprotium@gmail.com>
2023-02-05 18:46:06 +0000

    fix shell check disable

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -70,6 +70,6 @@ install_packages() {
 
 post_install_packages() {
   info "Installing fzf bindings"
-  # shellcheck disable=SC2155
+  # shellcheck disable=SC2046
   $(brew --prefix)/opt/fzf/install
 }
