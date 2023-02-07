636712b15082f1d9e7da55938085df8cc2d91c09
protiumx <ioprotium@gmail.com>
2023-02-07 21:55:11 +0100

    do not sneak in x,o modes

--- stow/nvim/.config/nvim/lua/config/vim-sneak.lua
+++ stow/nvim/.config/nvim/lua/config/vim-sneak.lua
@@ -3,8 +3,8 @@ local M = {}
 function M.setup()
   local colors = require('config.colors')
 
-  vim.keymap.set('', 's', '<Plug>Sneak_s', { remap = true })
-  vim.keymap.set('', 'S', '<Plug>Sneak_S', { remap = true })
+  vim.keymap.set({ 'n', 'v' }, 's', '<Plug>Sneak_s', { remap = true })
+  vim.keymap.set({ 'n', 'v' }, 'S', '<Plug>Sneak_S', { remap = true })
   vim.keymap.set('', 'f', '<Plug>Sneak_f', { remap = true })
   vim.keymap.set('', 'F', '<Plug>Sneak_F', { remap = true })
   vim.keymap.set('', 't', '<Plug>Sneak_t', { remap = true })
