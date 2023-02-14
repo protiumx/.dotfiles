518072e91db7029a330acff9370346cca4ae9207
protiumx <ioprotium@gmail.com>
2023-02-14 23:13:25 +0100

    formatting

--- stow/nvim/.config/nvim/lua/config/diffview.lua
+++ stow/nvim/.config/nvim/lua/config/diffview.lua
@@ -18,33 +18,31 @@ function M.setup()
         width = 40,
       },
     },
-
     file_history_panel = {
       win_config = { -- See ':h diffview-config-win_config'
         position = 'left',
         height = 40,
       },
     },
-
     keymaps = {
       view = {
-        { 'n', '<C-o>', actions.goto_file_tab, { desc = 'Open the file in a new tabpage' } },
-        { 'n', '<Leader>P', actions.focus_files, { desc = 'Bring focus to the file panel' } },
-        { 'n', '<Leader>p', actions.toggle_files, { desc = 'Toggle the file panel.' } },
+        { 'n', '<C-o>',     actions.goto_file_tab, { desc = 'Open the file in a new tabpage' } },
+        { 'n', '<Leader>P', actions.focus_files,   { desc = 'Bring focus to the file panel' } },
+        { 'n', '<Leader>p', actions.toggle_files,  { desc = 'Toggle the file panel.' } },
       },
 
       file_panel = {
-        { 'n', '<C-o>', actions.goto_file_tab, { desc = 'Open the file in a new tabpage' } },
-        { 'n', '<Leader>P', actions.focus_files, { desc = 'Bring focus to the file panel' } },
-        { 'n', '<Leader>p', actions.toggle_files, { desc = 'Toggle the file panel' } },
-        { 'n', 'q', '<cmd>DiffviewClose<CR>', { silent = true } },
+        { 'n', '<C-o>',     actions.goto_file_tab,    { desc = 'Open the file in a new tabpage' } },
+        { 'n', '<Leader>P', actions.focus_files,      { desc = 'Bring focus to the file panel' } },
+        { 'n', '<Leader>p', actions.toggle_files,     { desc = 'Toggle the file panel' } },
+        { 'n', 'q',         '<cmd>DiffviewClose<CR>', { silent = true } },
       },
 
       file_history_panel = {
-        { 'n', '<C-o>', actions.goto_file_tab, { desc = 'Open the file in a new tabpage' } },
-        { 'n', '<Leader>P', actions.focus_files, { desc = 'Bring focus to the file panel' } },
-        { 'n', '<Leader>p', actions.toggle_files, { desc = 'Toggle the file panel' } },
-        { 'n', 'q', '<cmd>DiffviewClose<CR>', { silent = true } },
+        { 'n', '<C-o>',     actions.goto_file_tab,    { desc = 'Open the file in a new tabpage' } },
+        { 'n', '<Leader>P', actions.focus_files,      { desc = 'Bring focus to the file panel' } },
+        { 'n', '<Leader>p', actions.toggle_files,     { desc = 'Toggle the file panel' } },
+        { 'n', 'q',         '<cmd>DiffviewClose<CR>', { silent = true } },
       },
     },
   })
