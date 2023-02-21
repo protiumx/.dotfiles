9af4b8eae4f6a4f7c106e8dd006890de3c241ede
protiumx <ioprotium@gmail.com>
2023-02-21 19:24:01 +0100

    command to get git file url

--- stow/nvim/.config/nvim/lua/config/commands.lua
+++ stow/nvim/.config/nvim/lua/config/commands.lua
@@ -1,15 +1,36 @@
 local M = {}
 
 local commands = {
-  toggle_format_on_save = function()
-    require('config.state')
+  git_file = function()
+    local remote = vim.fn.system('gremote')
+    local branch = vim.fn.system('git rev-parse --abbrev-ref HEAD')
+    print(string.format('%s/tree/%s/%s', remote, branch, vim.fn.expand('%')):gsub('[\n\r]', ''))
   end
 }
 
+local function load_command(cmd, ...)
+  local args = { ... }
+  if next(args) ~= nil then
+    commands[cmd](args[1])
+  else
+    commands[cmd]()
+  end
+end
+
+
 function M.load()
-  vim.api.nvim_create_user_command('Config', function(args)
-    require('lspsaga.command').load_command(unpack(args.fargs))
-  end, { desc = 'Toggle format on save' })
+  vim.api.nvim_create_user_command('X', function(args)
+    load_command(unpack(args.fargs))
+  end, {
+    desc = 'X command will help ya',
+    nargs = '+',
+    complete = function(arg)
+      local list = vim.tbl_keys(commands)
+      return vim.tbl_filter(function(s)
+        return string.match(s, '^' .. arg)
+      end, list)
+    end,
+  })
 end
 
 return M
