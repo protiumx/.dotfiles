9eff6172714952996411fa4ce7142c8a99629a27
protiumx <ioprotium@gmail.com>
2023-02-21 17:02:36 +0100

    create scratch term and expand path

--- stow/nvim/.config/nvim/lua/config/term.lua
+++ stow/nvim/.config/nvim/lua/config/term.lua
@@ -15,12 +15,28 @@ function M.setup()
   fterm.setup(term_opts)
 
   vim.api.nvim_create_user_command('TermExit', fterm.exit, { bang = true })
+
   vim.api.nvim_create_user_command('TermToggle', fterm.toggle, { bang = true })
+
   vim.api.nvim_create_user_command('TermRun', function(opts)
     fterm.run(opts.args)
   end, {
     bang = true,
     nargs = '+',
+  })
+
+  vim.api.nvim_create_user_command('TermScratch', function(opts)
+    local cmd = opts.args
+    if cmd:find('go test') then
+      cmd = string.gsub(opts.args, '%%', vim.fn.expand('%:h')) .. '/...'
+    end
+    fterm.scratch(vim.tbl_extend('force', {
+      cmd = cmd,
+    }, term_opts))
+  end, {
+    bang = true,
+    nargs = '+',
+    desc = 'Open scratch term with provided command'
   })
 end
 
