78ee8acf7e1fbdc74214b48a4211ed0080b148e4
Brian Mayo <ioprotium@gmail.com>
2023-02-21 00:22:52 +0000

    use global term to run commands

--- stow/nvim/.config/nvim/lua/config/term.lua
+++ stow/nvim/.config/nvim/lua/config/term.lua
@@ -2,18 +2,22 @@ local M = {}
 
 function M.setup()
   local fterm = require('FTerm')
+  local term_opts = {
+    hl = 'NormalFloat',
+    border = 'single',
+    dimensions = {
+      height = 0.3,
+      width = 0.2,
+      x = 1,
+      y = 0,
+    },
+  }
+  fterm.setup(term_opts)
 
-  vim.api.nvim_create_user_command('Scratch', function(opts)
-    fterm.scratch({
-      cmd = opts.args,
-      border = 'none',
-      dimensions = {
-        height = 0.2,
-        width = 0.3,
-        x = 1,
-        y = 0,
-      },
-    })
+  vim.api.nvim_create_user_command('TermExit', fterm.exit, { bang = true })
+  vim.api.nvim_create_user_command('TermToggle', fterm.toggle, { bang = true })
+  vim.api.nvim_create_user_command('TermRun', function(opts)
+    fterm.run(opts.args)
   end, {
     bang = true,
     nargs = '+',
