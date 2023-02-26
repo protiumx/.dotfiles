89efa731bf02cd1f18a400e8cab91d3b9bb2bd7d
Brian Mayo <ioprotium@gmail.com>
2023-02-26 23:53:55 +0000

    change cyan. set directory color

--- stow/nvim/.config/nvim/lua/config/colors.lua
+++ stow/nvim/.config/nvim/lua/config/colors.lua
@@ -1,10 +1,9 @@
 local colors = {
   background = '#1c1c1c',
   foreground = '#cacaca',
-
   accent = '#fe5186',
   blue = '#61afef',
-  cyan = '#56b6c2',
+  cyan = '#3e8fb0',
   dark_green = '#5faf5f',
   dark_grey = '#222222',
   dark_orange = '#ff5d62',
