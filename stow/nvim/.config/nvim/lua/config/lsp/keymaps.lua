9f5fbc470bc4c561b78c6801fefa4e6e0e056132
protiumx <ioprotium@gmail.com>
2023-03-24 22:18:49 +0100

    go to definition centered

--- stow/nvim/.config/nvim/lua/config/lsp/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/lsp/keymaps.lua
@@ -11,7 +11,7 @@ function M.setup(bufnr)
 
   nmap('gy', vim.lsp.buf.type_definition, 'Show type definition')
   nmap('gi', vim.lsp.buf.implementation, 'Go to implementation')
-  nmap('gd', vim.lsp.buf.definition, 'Go to definition')
+  nmap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>zz', 'Go to definition')
   nmap('K', vim.lsp.buf.hover, 'Show docs')
   nmap('<C-l>h', vim.lsp.buf.document_highlight, 'Highlight node')
   nmap('<C-l>c', vim.lsp.buf.document_highlight, 'Clear highlights')
