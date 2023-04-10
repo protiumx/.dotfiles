652d4f0014116709677f660a39c1f8926eea3519
Brian Mayo <ioprotium@gmail.com>
2023-04-10 21:57:51 +0000

    swap params keymaps

--- stow/nvim/.config/nvim/lua/config/treesitter.lua
+++ stow/nvim/.config/nvim/lua/config/treesitter.lua
@@ -11,8 +11,10 @@ local function setup_text_objects()
 
         keymaps = {
           -- You can use the capture groups defined in textobjects.scm
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
         },
         -- You can choose the select mode (default is charwise 'v')
         --
@@ -38,20 +40,30 @@ local function setup_text_objects()
         include_surrounding_whitespace = true,
       },
 
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
+      },
+
       move = {
         enable = true,
         set_jumps = true, -- whether to set jumps in the jumplist
         goto_next_start = {
-          ["]m"] = "@function.outer",
+          [']m'] = '@function.outer',
         },
         goto_next_end = {
-          ["]M"] = "@function.outer",
+          [']M'] = '@function.outer',
         },
         goto_previous_start = {
-          ["[m"] = "@function.outer",
+          ['[m'] = '@function.outer',
         },
         goto_previous_end = {
-          ["[M"] = "@function.outer",
+          ['[M'] = '@function.outer',
         },
       },
     }
