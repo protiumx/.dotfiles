61e337e5ecbcf2e008d52ae08fda64d08bf63859
protiumx <ioprotium@gmail.com>
2023-04-17 20:38:06 +0200

    utils: get visual selection text

---
+++ stow/nvim/.config/nvim/lua/config/utils.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+function M.get_vsel()
+  local vstart = vim.fn.getpos("'<")
+  local vend = vim.fn.getpos("'>")
+
+  local line_start = vstart[2]
+  local line_end = vend[2]
+
+  local lines = vim.fn.getline(line_start, line_end)
+  return lines and lines[0] or ''
+end
+
+function M.vtext()
+  local a_orig = vim.fn.getreg('a')
+  local mode = vim.fn.mode()
+  if mode ~= 'v' and mode ~= 'V' then
+    vim.cmd [[normal! gv]]
+  end
+  vim.cmd [[normal! "aygv]]
+  local text = vim.fn.getreg('a')
+  vim.fn.setreg('a', a_orig)
+  return text
+end
+
+return M
