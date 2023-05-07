98b9142c0c825f06d7028cf6f311b8096a0202f4
protiumx <ioprotium@gmail.com>
2023-05-07 19:11:13 +0200

    nvim tree config as module

--- stow/nvim/.config/nvim/lua/config/packer.lua
+++ stow/nvim/.config/nvim/lua/config/packer.lua
@@ -65,40 +65,7 @@ return packer.startup(function(use)
   use({
     'nvim-tree/nvim-tree.lua',
     config = function()
-      require("nvim-tree").setup({
-        hijack_unnamed_buffer_when_opening = false,
-        hijack_directories = {
-          enable = false,
-        },
-        renderer = {
-          icons = {
-            show = {
-              file = true,
-              folder = true,
-              folder_arrow = false,
-              git = false,
-              modified = false,
-            },
-            glyphs = {
-              folder = {
-                arrow_closed = "",
-                arrow_open = "",
-                default = "",
-                open = "",
-                empty = "",
-                empty_open = "",
-                symlink = "",
-                symlink_open = "",
-              },
-            }
-          }
-        },
-        update_focused_file = {
-          enable = true,
-          update_root = false,
-          ignore_list = {},
-        },
-      })
+      require('config.nvim-tree').setup()
     end,
     requires = {
       'nvim-tree/nvim-web-devicons',
