18d73bf4cf3aabbff81d32f47926b2f0dd2ec510
Brian Mayo <ioprotium@gmail.com>
2023-05-14 19:27:47 +0000

    restore gruvbox

--- stow/nvim/.config/nvim/after/plugin/colors.lua
+++ stow/nvim/.config/nvim/after/plugin/colors.lua
@@ -3,32 +3,29 @@ local themes = require('config.themes')
 -- local photon = require('config.themes.photon')
 
 
-vim.cmd("colorscheme PaperColor")
-vim.g['PaperColor_Theme_Options'] = {
-  theme = {
-    default = { dark = { override = { color07 = { colors.foreground, '' } } } }
-  }
-}
+-- vim.cmd("colorscheme PaperColor")
+-- vim.g['PaperColor_Theme_Options'] = {
+--   theme = {
+--     default = { dark = { override = { color07 = { colors.foreground, '' } } } }
+--   }
+-- }
 
-vim.schedule(function()
-  themes.setup()
-  themes.load(true)
-end)
--- require("gruvbox").setup({
---   undercurl = true,
---   underline = true,
---   bold = true,
---   palette_overrides = {
---     dark0 = colors.background,
---     dark1 = colors.background,
---   },
---   overrides = {
---     Pmenu = { bg = colors.dark_grey },
---   },
---   dim_inactive = true,
---   transparent_mode = false,
--- })
--- vim.cmd("colorscheme gruvbox")
+themes.setup()
+require("gruvbox").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  palette_overrides = {
+    dark0 = colors.background,
+    dark1 = colors.background,
+  },
+  overrides = {
+    Pmenu = { bg = colors.dark_grey },
+  },
+  dim_inactive = true,
+  transparent_mode = false,
+})
+vim.cmd("colorscheme gruvbox")
 
--- themes.load(false)
+themes.load(false)
 -- photon.load()
