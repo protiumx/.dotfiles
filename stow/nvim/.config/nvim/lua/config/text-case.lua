6385b3b0ea363a7e035b67cd989f33fafb03c1b7
protiumx <ioprotium@gmail.com>
2023-06-09 22:19:36 +0200

    remove unused

--- stow/nvim/.config/nvim/lua/config/text-case.lua
+++ stow/nvim/.config/nvim/lua/config/text-case.lua
@@ -9,12 +9,9 @@ function M.setup()
   vim.keymap.set('n', 'gas', function() textcase.operator('to_snake_case') end, { silent = true })
   vim.keymap.set('n', 'gad', function() textcase.operator('to_dash_case') end, { silent = true })
   vim.keymap.set('n', 'gan', function() textcase.operator('to_constant_case') end, { silent = true })
-  vim.keymap.set('n', 'gad', function() textcase.operator('to_dot_case') end, { silent = true })
-  vim.keymap.set('n', 'gaa', function() textcase.operator('to_phrase_case') end, { silent = true })
+  vim.keymap.set('n', 'gaD', function() textcase.operator('to_dot_case') end, { silent = true })
   vim.keymap.set('n', 'gac', function() textcase.operator('to_camel_case') end, { silent = true })
   vim.keymap.set('n', 'gap', function() textcase.operator('to_pascal_case') end, { silent = true })
-  vim.keymap.set('n', 'gat', function() textcase.operator('to_title_case') end, { silent = true })
-  vim.keymap.set('n', 'gaf', function() textcase.operator('to_path_case') end, { silent = true })
 end
 
 return M
