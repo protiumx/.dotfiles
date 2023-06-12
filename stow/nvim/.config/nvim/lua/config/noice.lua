6c35be8d8d5229846cfea047bf1a1243e6681854
protiumx <ioprotium@gmail.com>
2023-06-12 13:20:10 +0200

    fix command popup position

--- stow/nvim/.config/nvim/lua/config/noice.lua
+++ stow/nvim/.config/nvim/lua/config/noice.lua
@@ -25,7 +25,7 @@ function M.setup()
       popupmenu = {
         relative = 'editor',
         position = {
-          row = '58%',
+          row = '64%',
           col = '50%',
         },
         size = {
@@ -33,24 +33,7 @@ function M.setup()
           height = 'auto',
         },
         border = {
-          padding = { 0, 1 },
-        },
-        win_options = {
-          winhighlight = { Normal = 'NormalFloat', FloatBorder = 'FloatBorder' },
-        },
-      },
-      confirm = {
-        relative = 'editor',
-        position = {
-          row = '88%',
-          col = '50%',
-        },
-        size = {
-          width = 60,
-          height = 'auto',
-        },
-        border = {
-          padding = { 0, 1 },
+          padding = { 1, 2 },
         },
         win_options = {
           winhighlight = { Normal = 'NormalFloat', FloatBorder = 'FloatBorder' },
@@ -91,7 +74,6 @@ function M.setup()
     -- you can enable a preset for easier configuration
     presets = {
       bottom_search = false,        -- use a classic bottom cmdline for search
-      command_palette = true,       -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false,           -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = true,        -- add a border to hover docs and signature help
