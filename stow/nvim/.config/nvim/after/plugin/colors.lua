f02c58e1a9e52002e0f8e2ae9a0605af410437e3
protiumx <ioprotium@gmail.com>
2023-06-17 17:02:11 +0200

    use gruvbox

--- stow/nvim/.config/nvim/after/plugin/colors.lua
+++ stow/nvim/.config/nvim/after/plugin/colors.lua
@@ -1,27 +1,22 @@
 local colors = require('config.colors')
 
--- require('gruvbox').setup({
---   undercurl = true,
---   underline = true,
---   bold = true,
---   palette_overrides = {
---     dark0 = colors.background,
---     dark1 = colors.background,
---   },
---   overrides = {
---     NotifyINFOIcon = { link = 'GruvboxFg1' },
---   },
---   dim_inactive = true,
---   transparent_mode = false,
--- })
---
-
-require("tokyonight").setup({
-  transparent = true,
+require('gruvbox').setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  palette_overrides = {
+    dark0 = colors.background,
+    dark1 = colors.background,
+  },
+  dim_inactive = false,
+  transparent_mode = false,
 })
 
--- vim.cmd('colorscheme gruvbox')
-vim.cmd('colorscheme tokyonight-storm')
+-- require("tokyonight").setup({
+--   transparent = true,
+-- })
+
+vim.cmd('colorscheme gruvbox')
 
 colors.load()
 
