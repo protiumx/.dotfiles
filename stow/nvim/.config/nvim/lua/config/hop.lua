6dac3ed6837472b94ab0b703cc933122a8c54665
protiumx <ioprotium@gmail.com>
2023-07-12 19:18:39 +0200

    fix tT offset

--- stow/nvim/.config/nvim/lua/config/hop.lua
+++ stow/nvim/.config/nvim/lua/config/hop.lua
@@ -29,14 +29,17 @@ function M.setup()
   vim.keymap.set('', 'f', function()
     hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
   end, { remap = true })
+
   vim.keymap.set('', 'F', function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = -1 })
   end, { remap = true })
+
   vim.keymap.set('', 't', function()
     hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
   end, { remap = true })
+
   vim.keymap.set('', 'T', function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = -1 })
   end, { remap = true })
 end
 
