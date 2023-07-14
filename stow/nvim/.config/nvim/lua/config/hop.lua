855d221b48be8547d5a12e015375a167bf088ac3
protiumx <ioprotium@gmail.com>
2023-07-14 18:47:53 +0200

    fix t and f

--- stow/nvim/.config/nvim/lua/config/hop.lua
+++ stow/nvim/.config/nvim/lua/config/hop.lua
@@ -31,15 +31,15 @@ function M.setup()
   end, { remap = true })
 
   vim.keymap.set('', 'F', function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = -1 })
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
   end, { remap = true })
 
   vim.keymap.set('', 't', function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
   end, { remap = true })
 
   vim.keymap.set('', 'T', function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = -1 })
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
   end, { remap = true })
 end
 
