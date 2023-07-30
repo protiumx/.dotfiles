0d0ac136edbdaa707c5891e2feab68e09b59d7d1
Brian Mayo <ioprotium@gmail.com>
2023-07-30 23:41:15 +0000

    use on_attach from lspconfig

--- stow/nvim/.config/nvim/lua/config/lsp/mason.lua
+++ stow/nvim/.config/nvim/lua/config/lsp/mason.lua
@@ -147,18 +147,19 @@ function M.setup()
       require('lspconfig')[server_name].setup {
         capabilities = capabilities,
         settings = servers[server_name],
+        on_attach = on_lsp_attach,
       }
     end,
   }
 
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(args)
-      local bufnr = args.buf
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      on_lsp_attach(client, bufnr)
-    end,
-  })
+  -- vim.api.nvim_create_autocmd("LspAttach", {
+  --   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  --   callback = function(args)
+  --     local bufnr = args.buf
+  --     local client = vim.lsp.get_client_by_id(args.data.client_id)
+  --     on_lsp_attach(client, bufnr)
+  --   end,
+  -- })
 end
 
 return M
