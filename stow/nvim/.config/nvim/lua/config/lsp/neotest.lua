4e78e9d1fde70101b46deefe20e9ba4c0182b2bc
Brian Mayo <ioprotium@gmail.com>
2023-08-03 23:59:35 +0000

    neotest config

---
+++ stow/nvim/.config/nvim/lua/config/lsp/neotest.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+function M.setup()
+  -- get neotest namespace (api call creates or returns namespace)
+  local neotest_ns = vim.api.nvim_create_namespace("neotest")
+  vim.diagnostic.config({
+    virtual_text = {
+      format = function(diagnostic)
+        local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+        return message
+      end,
+    },
+  }, neotest_ns)
+
+  require("neotest").setup({
+    adapters = {
+      require("neotest-go"),
+    },
+    icons = {
+      child_indent = "│",
+      child_prefix = "├",
+      collapsed = "─",
+      expanded = "╮",
+      failed = "",
+      final_child_indent = " ",
+      final_child_prefix = "╰",
+      non_collapsible = "─",
+      passed = "",
+      running = "",
+      running_animated = { "/", "|", "\\", "-", "/", "|", "\\", "-" },
+      skipped = "",
+      unknown = "",
+      watching = ""
+    },
+  })
+end
+
+return M
