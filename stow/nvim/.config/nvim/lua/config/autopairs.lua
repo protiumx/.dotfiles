893d604a83819f3db772eee0767cb7befe6a187d
Brian Mayo <ioprotium@gmail.com>
2023-08-13 00:28:51 +0000

    disable autopairs in nvim-cmp

--- stow/nvim/.config/nvim/lua/config/autopairs.lua
+++ stow/nvim/.config/nvim/lua/config/autopairs.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 function M.setup()
-  local cmp = require('cmp')
-  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+  -- local cmp = require('cmp')
+  -- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
   require('nvim-autopairs').setup({
     fast_wrap = {},
@@ -10,8 +10,8 @@ function M.setup()
     check_ts = true,
   })
 
-  -- Add parens to functions
-  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  -- -- Add parens to functions
+  -- cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 end
 
 return M
