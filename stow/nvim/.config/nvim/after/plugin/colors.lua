02a43c01d56e08260ee54be66cee5a3c8f47aca9
protiumx <ioprotium@gmail.com>
2023-08-25 20:45:57 +0200

    gruvbox addiction

--- stow/nvim/.config/nvim/after/plugin/colors.lua
+++ stow/nvim/.config/nvim/after/plugin/colors.lua
@@ -1,19 +1,19 @@
 local colors = require('config.colors')
 
--- require('gruvbox').setup({
---   undercurl = true,
---   underline = true,
---   bold = true,
---   contrast = "",
---   palette_overrides = {
---     dark0 = colors.background,
---     dark1 = colors.background,
---   },
---   dim_inactive = false,
---   transparent_mode = true,
--- })
+require('gruvbox').setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  contrast = "",
+  palette_overrides = {
+    dark0 = colors.background,
+    dark1 = colors.background,
+  },
+  dim_inactive = false,
+  transparent_mode = true,
+})
 
-vim.cmd('colorscheme zenwritten')
+vim.cmd('colorscheme gruvbox')
 
 
 -- vim.cmd [[colorscheme flatwhite]]
