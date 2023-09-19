d75dc4006ce73521b92d38006409695dde5eec67
protiumx <ioprotium@gmail.com>
2023-09-19 22:27:59 +0200

    use zenbones zenwritten

--- stow/nvim/.config/nvim/after/plugin/colors.lua
+++ stow/nvim/.config/nvim/after/plugin/colors.lua
@@ -1,20 +1,20 @@
 local colors = require('config.colors')
 
-require('gruvbox').setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  contrast = "",
-  palette_overrides = {
-    dark0 = colors.background,
-    dark1 = colors.background,
-  },
-  dim_inactive = false,
-  transparent_mode = true,
-})
+-- require('gruvbox').setup({
+--   undercurl = true,
+--   underline = true,
+--   bold = true,
+--   contrast = "",
+--   palette_overrides = {
+--     dark0 = colors.background,
+--     dark1 = colors.background,
+--   },
+--   dim_inactive = false,
+--   transparent_mode = true,
+-- })
 
-vim.cmd('colorscheme gruvbox')
+-- vim.cmd('colorscheme gruvbox')
 
 
--- vim.cmd [[colorscheme flatwhite]]
+vim.cmd [[colorscheme zenwritten]]
 colors.load()
