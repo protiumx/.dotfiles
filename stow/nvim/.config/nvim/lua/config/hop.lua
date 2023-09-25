6c59a5877ee364b5ca1b8e47724250505bb724a2
protiumx <ioprotium@gmail.com>
2023-09-25 16:50:40 +0200

    use hops only on normal and visual

--- stow/nvim/.config/nvim/lua/config/hop.lua
+++ stow/nvim/.config/nvim/lua/config/hop.lua
@@ -26,19 +26,19 @@ function M.setup()
     hop.hint_patterns({ direction = directions.AFTER_CURSOR, multi_windows = true })
   end, { remap = true })
 
-  vim.keymap.set('', 'f', function()
+  vim.keymap.set({ 'n', 'v' }, 'f', function()
     hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
   end, { remap = true })
 
-  vim.keymap.set('', 'F', function()
+  vim.keymap.set({ 'n', 'v' }, 'F', function()
     hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
   end, { remap = true })
 
-  vim.keymap.set('', 't', function()
+  vim.keymap.set({ 'n', 'v' }, 't', function()
     hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
   end, { remap = true })
 
-  vim.keymap.set('', 'T', function()
+  vim.keymap.set({ 'n', 'v' }, 'T', function()
     hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
   end, { remap = true })
 end
