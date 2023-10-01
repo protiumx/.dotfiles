af872bb9821b31eb2e677ed4e2b1be7b3abdf3ec
Brian Mayo <ioprotium@gmail.com>
2023-10-01 21:16:15 +0000

    disable f and t

--- stow/nvim/.config/nvim/lua/config/hop.lua
+++ stow/nvim/.config/nvim/lua/config/hop.lua
@@ -26,21 +26,21 @@ function M.setup()
     hop.hint_patterns({ direction = directions.AFTER_CURSOR, multi_windows = true })
   end, { remap = true })
 
-  vim.keymap.set({ 'n', 'v' }, 'f', function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-  end, { remap = true })
+  -- vim.keymap.set({ 'n', 'v' }, 'f', function()
+  --   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+  -- end, { remap = true })
 
-  vim.keymap.set({ 'n', 'v' }, 'F', function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-  end, { remap = true })
+  -- vim.keymap.set({ 'n', 'v' }, 'F', function()
+  --   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+  -- end, { remap = true })
 
-  vim.keymap.set({ 'n', 'v' }, 't', function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-  end, { remap = true })
+  -- vim.keymap.set({ 'n', 'v' }, 't', function()
+  --   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+  -- end, { remap = true })
 
-  vim.keymap.set({ 'n', 'v' }, 'T', function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-  end, { remap = true })
+  -- vim.keymap.set({ 'n', 'v' }, 'T', function()
+  --   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+  -- end, { remap = true })
 end
 
 return M
