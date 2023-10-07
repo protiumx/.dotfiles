296ea5f5db78af4a55f2ccd026acb308111929c6
protiumx <ioprotium@gmail.com>
2023-10-07 16:36:45 +0200

    keymap generate uuid

--- stow/nvim/.config/nvim/lua/config/lualine.lua
+++ stow/nvim/.config/nvim/lua/config/lualine.lua
@@ -86,6 +86,7 @@ function M.setup()
       disabled_filetypes = {
         statusline = {
           -- 'help',
+          'FTerm',
           'lspsagaoutline',
           'packer',
           'noice',
