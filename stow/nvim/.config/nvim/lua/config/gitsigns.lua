8abe812e8e76322c13184024ef863091e7674659
protiumx <ioprotium@gmail.com>
2023-10-20 17:57:39 +0200

    call local gs

--- stow/nvim/.config/nvim/lua/config/gitsigns.lua
+++ stow/nvim/.config/nvim/lua/config/gitsigns.lua
@@ -30,7 +30,7 @@ local function on_attach(bufnr)
   map('n', '<C-g>td', gs.toggle_deleted, '[Git] Toggle deleted preview')
   map('n', '<C-g>hr', gs.reset_hunk, '[Git] Reset hunk')
   map('v', '<C-g>hr', function() gs.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end)
-  map('n', '<C-g>hp', '<cmd>Gitsigns preview_hunk<CR>', '[Git] Blame line short')
+  map('n', '<C-g>hp', gs.preview_hunk, '[Git] Preview hunk')
   map('n', '<C-g>br', gs.reset_buffer)
 
   -- Text object
