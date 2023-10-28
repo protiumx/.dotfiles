ede8d7dbb02310e8600739217c0f6b4f895da2fd
protiumx <ioprotium@gmail.com>
2023-10-29 00:45:33 +0200

    indent blank line and themes

--- stow/nvim/.config/nvim/lua/config/packer.lua
+++ stow/nvim/.config/nvim/lua/config/packer.lua
@@ -101,6 +101,14 @@ return packer.startup(function(use)
   })
 
   use({
+    "lukas-reineke/indent-blankline.nvim",
+  })
+
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use "savq/melange-nvim"
+  use "folke/tokyonight.nvim"
+
+  use({
     'nvim-treesitter/playground',
     cmd = { 'TSPlaygroundToggle', 'TSNodeUnderCursor' },
   })
