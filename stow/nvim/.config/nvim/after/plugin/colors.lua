a8514795e77a3c9a6f2e520a0413257720f401cf
protiumx <ioprotium@gmail.com>
2023-10-29 00:42:29 +0200

    enable indent line; gruvbox

--- stow/nvim/.config/nvim/after/plugin/colors.lua
+++ stow/nvim/.config/nvim/after/plugin/colors.lua
@@ -1,20 +1,32 @@
 local colors = require('config.colors')
 
--- require('gruvbox').setup({
---   undercurl = true,
---   underline = true,
---   bold = true,
---   contrast = "",
---   palette_overrides = {
---     dark0 = colors.background,
---     dark1 = colors.background,
---   },
---   dim_inactive = false,
---   transparent_mode = true,
--- })
+require('gruvbox').setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  contrast = "",
+  palette_overrides = {
+    dark0 = colors.background,
+    dark1 = colors.background,
+  },
+  dim_inactive = false,
+  transparent_mode = true,
+})
 
--- vim.cmd('colorscheme gruvbox')
+vim.cmd('colorscheme gruvbox')
 
 
-vim.cmd [[colorscheme zenwritten]]
+-- vim.cmd [[colorscheme kanagawa]]
 colors.load()
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "Transparent", { fg = "none", bg = 'none' })
+end)
+
+require("ibl").setup({
+  indent = { char = " " },
+  scope = { enabled = true, char = "▏", highlight = { "NonText" }, show_start = false, show_end = false }
+})
