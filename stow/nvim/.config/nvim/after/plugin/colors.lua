a1d89828835153179d69d200ed94aa066d188519
Brian Mayo <ioprotium@gmail.com>
2023-11-01 22:32:10 +0000

    indent line not that pretty

--- stow/nvim/.config/nvim/after/plugin/colors.lua
+++ stow/nvim/.config/nvim/after/plugin/colors.lua
@@ -24,14 +24,9 @@ vim.cmd('colorscheme sonokai')
 -- vim.cmd [[colorscheme kanagawa]]
 colors.load()
 
-local hooks = require "ibl.hooks"
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, "Transparent", { fg = "none", bg = 'none' })
-end)
+-- vim.api.nvim_set_hl(0, "RedRed", { fg = '#c04c0d', bg = 'none' })
 
-require("ibl").setup({
-  indent = { char = " " },
-  scope = { enabled = true, char = "│", highlight = { "NonText" }, show_start = false, show_end = false }
-})
+-- require("ibl").setup({
+--   indent = { char = "⁚", highlight = { "NonText" } },
+--   scope = { enabled = true, char = "│", highlight = { "RedRed" }, show_start = false, show_end = false }
+-- })
