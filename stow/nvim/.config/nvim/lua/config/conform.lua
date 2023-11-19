fb9754fa057c16e1e7e27958748ea6951900bae8
Brian Mayo <ioprotium@gmail.com>
2023-11-19 20:50:02 +0000

    setup autosave

--- stow/nvim/.config/nvim/lua/config/conform.lua
+++ stow/nvim/.config/nvim/lua/config/conform.lua
@@ -3,6 +3,10 @@ local M = {}
 function M.setup()
   local conform = require('conform')
   conform.setup({
+    format_on_save = {
+      timeout_ms = 1000,
+      lsp_fallback = true,
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { 'black' },
