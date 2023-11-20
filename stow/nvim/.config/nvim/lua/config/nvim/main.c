49ec8e461d40fe6e1f11dc67af30ec33cc69a7a3
Brian Mayo <ioprotium@gmail.com>
2023-11-20 23:15:25 +0000

    fix str copy and free initial filename

--- stow/nvim/.config/nvim/lua/config/nvim/main.c
+++ stow/nvim/.config/nvim/lua/config/nvim/main.c
@@ -14,34 +14,41 @@ extern char *get_cursor_line_ptr(void);
 // Expects format file:line:column
 char *file_under_cursor() {
   int32_t lnum = -1;
-  char *ptr = grab_file_name(1, &lnum);
-  if (ptr == NULL) {
+  char *file_name = grab_file_name(1, &lnum);
+  if (file_name == NULL) {
     return NULL;
   }
 
   if (lnum == -1) {
-    return ptr;
+    return file_name;
   }
 
-  const int len = strlen(ptr);
-  char *start = strstr(get_cursor_line_ptr(), ptr);
-  int size = 0;
-  char c = start[size];
+  const int file_name_len = strlen(file_name);
+  // position pointer at the end of the file_name in the current line
+  char *pos_ptr = strstr(get_cursor_line_ptr(), file_name) + strlen(file_name);
+  int pos_size = 0;
+  char c = *pos_ptr;
   while (c != '\0' && c != ' ' && c != '\t') {
-    size++;
-    c = start[size];
+    pos_size++;
+    c = pos_ptr[pos_size];
   }
 
-  if (size == 0) {
-    // Use lnum, format might be `file line xxx`
-    char *ret;
-    sprintf(ret, "%s:%d", ptr, lnum);
+  char *ret = NULL;
+  if (pos_size == 0) {
+    // Use lnum
+    // allocate for ":" + lnum + \0, assume 4 digits is enough
+    ret = (char *)malloc(file_name_len + 6);
+    sprintf(ret, "%s:%d", file_name, lnum);
+    free(file_name);
+    return ret;
   }
 
-  char *ret = (char *)malloc(len + size + 1);
-  memcpy(ret, start, len + size);
+  ret = (char *)malloc(file_name_len + pos_size + 1);
+  strncpy(ret, file_name, file_name_len);
+  strncpy(ret + file_name_len, pos_ptr, pos_size);
+  ret[file_name_len + pos_size] = '\0';
 
-  ret[len + size] = '\0';
+  free(file_name);
 
   return ret;
 }
