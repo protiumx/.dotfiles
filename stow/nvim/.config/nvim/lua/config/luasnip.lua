acc56afb6f5e8da0553185fa3b41b8c3f98ab2ac
Brian Mayo <ioprotium@gmail.com>
2023-11-28 13:47:28 +0000

    move cmp out of lsp folder

--- stow/nvim/.config/nvim/lua/config/luasnip.lua
+++ stow/nvim/.config/nvim/lua/config/luasnip.lua
@@ -7,8 +7,8 @@ function M.setup()
   local t = luasnip.text_node
 
   luasnip.config.set_config({
-    region_check_events = 'InsertEnter',
     delete_check_events = 'InsertLeave',
+    region_check_events = 'InsertEnter',
   })
 
   require('luasnip.loaders.from_vscode').lazy_load()
