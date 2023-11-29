a31affc1fe3f09b60bbc9b16eb7bc8b5d00f34a3
protiumx <ioprotium@gmail.com>
2023-11-29 12:39:15 +0100

    use icons module

--- stow/nvim/.config/nvim/lua/config/lsp/init.lua
+++ stow/nvim/.config/nvim/lua/config/lsp/init.lua
@@ -1,10 +1,12 @@
+local icons = require('config.icons').lsp
+
 local M = {}
 
 function M.setup()
-  vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
-  vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
-  vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
-  vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+  vim.fn.sign_define('DiagnosticSignError', { text = icons.error, texthl = 'DiagnosticSignError' })
+  vim.fn.sign_define('DiagnosticSignWarn', { text = icons.warn, texthl = 'DiagnosticSignWarn' })
+  vim.fn.sign_define('DiagnosticSignInfo', { text = icons.info, texthl = 'DiagnosticSignInfo' })
+  vim.fn.sign_define('DiagnosticSignHint', { text = icons.hint, texthl = 'DiagnosticSignHint' })
 
   vim.lsp.set_log_level('off')
 
