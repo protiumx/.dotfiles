6859644ddb1a0c65db815da8a149671bfb3f8878
Brian Mayo <ioprotium@gmail.com>
2023-11-30 23:42:58 +0000

    remove unused. more choises for errors

--- stow/nvim/.config/nvim/lua/config/snippets/go.lua
+++ stow/nvim/.config/nvim/lua/config/snippets/go.lua
@@ -1,6 +1,7 @@
 local ls = require('luasnip')
-
-local snippet_from_nodes = ls.sn
+local rep = require('luasnip.extras').rep
+local fmta = require('luasnip.extras.fmt').fmta
+local fmt = require('luasnip.extras.fmt').fmt
 
 local s = ls.s
 local sn = ls.snippet_node
@@ -8,183 +9,47 @@ local i = ls.insert_node
 local t = ls.text_node
 local d = ls.dynamic_node
 local c = ls.choice_node
-local fmta = require('luasnip.extras.fmt').fmta
-local rep = require('luasnip.extras').rep
 
-local ts_locals = require('nvim-treesitter.locals')
-local ts_utils = require('nvim-treesitter.ts_utils')
-
-local get_node_text = vim.treesitter.get_node_text
-
-local transforms = {
-  int = function(_, _)
-    return t('0')
-  end,
-
-  bool = function(_, _)
-    return t('false')
-  end,
-
-  string = function(_, _)
-    return t([[""]])
-  end,
-
-  error = function(_, info)
-    if info then
-      info.index = info.index + 1
-
-      return c(info.index, {
-        t(info.err_name),
-        t(string.format('errors.Wrap(%s, "%s")', info.err_name, info.func_name)),
-      })
-    else
-      return t('err')
-    end
-  end,
-
-  -- Types with a "*" mean they are pointers, so return nil
-  [function(text)
-    return string.find(text, '*', 1, true) ~= nil
-  end] = function(_, _)
-    return t('nil')
-  end,
+local snip = [[
+<val>, <err> := <func>(<args>)
+if <err_r> != nil {
+  return <ret>
 }
-
-local transform = function(text, info)
-  local condition_matches = function(condition, ...)
-    if type(condition) == 'string' then
-      return condition == text
-    else
-      return condition(...)
-    end
-  end
-
-  for condition, result in pairs(transforms) do
-    if condition_matches(condition, text, info) then
-      return result(text, info)
-    end
-  end
-
-  return t(text)
-end
-
-local handlers = {
-  parameter_list = function(node, info)
-    local result = {}
-
-    local count = node:named_child_count()
-    for idx = 0, count - 1 do
-      local matching_node = node:named_child(idx)
-      local type_node = matching_node:field('type')[1]
-      table.insert(result, transform(get_node_text(type_node, 0), info))
-      if idx ~= count - 1 then
-        table.insert(result, t({ ', ' }))
-      end
-    end
-
-    return result
-  end,
-
-  type_identifier = function(node, info)
-    local text = get_node_text(node, 0)
-    return { transform(text, info) }
-  end,
-}
-
-local function_node_types = {
-  function_declaration = true,
-  method_declaration = true,
-  func_literal = true,
-}
-
-local function go_result_type(info)
-  local cursor_node = ts_utils.get_node_at_cursor()
-  local scope = ts_locals.get_scope_tree(cursor_node, 0)
-
-  local function_node
-  for _, v in ipairs(scope) do
-    if function_node_types[v:type()] then
-      function_node = v
-      break
-    end
-  end
-
-  if not function_node then
-    print('Not inside of a function')
-    return t('')
-  end
-
-  local query = vim.treesitter.query.parse(
-    'go',
-    [[
-      [
-        (method_declaration result: (_) @id)
-        (function_declaration result: (_) @id)
-        (func_literal result: (_) @id)
-      ]
-    ]]
-  )
-
-  for _, node in query:iter_captures(function_node, 0) do
-    print('iter caps', vim.inspect(node))
-    if handlers[node:type()] then
-      return handlers[node:type()](node, info)
-    end
-  end
-
-  return t('')
-end
-
-local go_ret_vals = function(args)
-  local err_name = args[1][1]
-  local func_name = args[2][1]
-  return snippet_from_nodes(
-    nil,
-    c(nil, {
-      t(err_name),
-      t(string.format('errors.Wrap(%s, "%s")', err_name, func_name)),
-    })
-  )
-end
-
-local fmt = require('luasnip.extras.fmt').fmt
+<finish>
+]]
 
 return {
   setup = function()
     ls.add_snippets('go', {
       s(
         'efi',
-        fmta(
-          [[
-<val>, <err> := <f>(<args>)
-if <err_same> != nil {
-  return <result>
-}
-<finish>
-]],
-          {
-            val = i(1),
-            err = i(2, 'err'),
-            f = i(3),
-            args = i(4),
-            err_same = rep(2),
-            result = d(5, function(args)
-              local name = (args and args[1] and args[1][1]) or ''
-              print(name)
-              return sn(
-                nil,
-                c(1, {
-                  t(name),
-                  fmt('fmt.Error("{}: %v", {})', {
-                    i(1, 'failed'),
-                    t(name),
-                  }),
-                })
-              )
-            end, { 2 }),
-            finish = i(0),
-          }
-        )
+        fmta(snip, {
+          val = i(1),
+          err = i(2, 'err'),
+          func = i(3),
+          args = i(4),
+          err_r = rep(2),
+          ret = d(5, function(args)
+            local err = (args and args[1] and args[1][1]) or ''
+            return sn(
+              nil,
+              c(1, {
+                t(err),
+
+                fmt('fmt.Error("{}: %v", {})', {
+                  i(1, 'failed'),
+                  t(err),
+                }),
+
+                fmt('errors.Wrap({}, "{}")', {
+                  t(err),
+                  i(1, 'failed'),
+                }),
+              })
+            )
+          end, { 2 }),
+          finish = i(0),
+        })
       ),
     })
   end,
