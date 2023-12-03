9405e788419d7105262b374d89cf0a19fd05d402
Brian Mayo <ioprotium@gmail.com>
2023-12-03 16:33:06 +0000

    better luasnip local jump

--- stow/nvim/.config/nvim/lua/config/cmp.lua
+++ stow/nvim/.config/nvim/lua/config/cmp.lua
@@ -180,8 +180,8 @@ function M.setup()
       ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif luasnip.expandable() then
-          luasnip.expand()
+        elseif luasnip.expand_or_locally_jumpable() then
+          luasnip.expand_or_jump()
         else
           fallback()
         end
