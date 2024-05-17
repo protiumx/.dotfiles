73f537ac0d3e1829b1b71725acd33a05baddaf4c
Brian Mayo <ioprotium@gmail.com>
2024-05-17 20:32:42 +0000

    add zinit and align links

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -8,35 +8,35 @@ taps=(
 )
 
 packages=(
-	bat       #  https://github.com/sharkdp/bat
-	bandwhich #  https://github.com/imsnif/bandwhich
-	bottom    #  https://github.com/ClementTsang/bottom
+	bat        # https://github.com/sharkdp/bat
+	bandwhich  # https://github.com/imsnif/bandwhich
+	bottom     # https://github.com/ClementTsang/bottom
 	cmake
 	ctags
 	curl
-	dasel #  https://github.com/TomWright/dasel
-	dust  #  https://github.com/bootandy/dust
-	eza   #  https://github.com/eza-community/eza
-	fzf   #  https://github.com/junegunn/fzf
-	fd    #  https://github.com/sharkdp/fd
+	dasel      # https://github.com/TomWright/dasel
+	dust       # https://github.com/bootandy/dust
+	eza        # https://github.com/eza-community/eza
+	fzf        # https://github.com/junegunn/fzf
+	fd         # https://github.com/sharkdp/fd
 	gettext
-	git-delta #  https://github.com/dandavison/delta
+	git-delta  # https://github.com/dandavison/delta
 	gpg
 	go
-	graphviz #  https://graphviz.org/
-	grpcurl  #  https://github.com/fullstorydev/grpcurl
-	httpie   #  https://github.com/httpie/httpie
+	graphviz   # https://graphviz.org/
+	grpcurl    # https://github.com/fullstorydev/grpcurl
+	httpie     # https://github.com/httpie/httpie
 	imagemagick
-	jless #  https://github.com/PaulJuliusMartinez/jless
+	jless      # https://github.com/PaulJuliusMartinez/jless
 	jq
-	k9s #  https://github.com/derailed/k9s
+	k9s        # https://github.com/derailed/k9s
 	kubernetes-cli
-	hyperfine  #  https://github.com/sharkdp/hyperfine
-	lazydocker #  https://github.com/jesseduffield/lazydocker
-	lf         #  https://github.com/gokcehan/lf
+	hyperfine  # https://github.com/sharkdp/hyperfine
+	lazydocker # https://github.com/jesseduffield/lazydocker
+	lf         # https://github.com/gokcehan/lf
 	libpq
-	lynx #  https://lynx.invisible-island.net/
-	mas  #  https://github.com/mas-cli/mas
+	lynx       # https://lynx.invisible-island.net/
+	mas        # https://github.com/mas-cli/mas
 	minikube
 	neovim
 	node
@@ -45,23 +45,22 @@ packages=(
 	openssl
 	pinentry-mac
 	postgresql
-	procs #  https://github.com/dalance/procs/
+	procs      # https://github.com/dalance/procs/
 	python3
 	protobuf
-	ripgrep #  https://github.com/BurntSushi/ripgre
+	ripgrep    # https://github.com/BurntSushi/ripgre
 	rustup
-	sd #  https://github.com/chmln/sd
+	sd         # https://github.com/chmln/sd
 	shellcheck
 	stow
-	stylua #  https://github.com/JohnnyMorganz/StyLua
+	stylua     # https://github.com/JohnnyMorganz/StyLua
 	telnet
-	usql     #  https://github.com/xo/usql
-	websocat #  https://github.com/vi/websocat
+	usql       # https://github.com/xo/usql
+	websocat   # https://github.com/vi/websocat
 	wget
 	zsh
-	zsh-autosuggestions
-	zsh-syntax-highlighting
-	zoxide #  https://github.com/ajeetdsouza/zoxide
+  zinit      # https://github.com/zdharma-continuum/zinit
+	zoxide     # https://github.com/ajeetdsouza/zoxide
 )
 
 install_packages() {
