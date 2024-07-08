8c235e3ecf68693d3c3dbe5faf72b85a0a929b88
Brian Mayo <ioprotium@gmail.com>
2024-07-08 22:38:28 +0000

    better clangd flags

--- stow/nvim/.config/nvim/lua/config/lsp/servers.lua
+++ stow/nvim/.config/nvim/lua/config/lsp/servers.lua
@@ -38,7 +38,25 @@ local servers = {
       },
     },
   },
-  clangd = {},
+  clangd = {
+    cmd = {
+      'clangd',
+      '-j=6',
+      '--all-scopes-completion',
+      '--background-index', -- should include a compile_commands.json or .txt
+      '--clang-tidy',
+      '--cross-file-rename',
+      '--completion-style=detailed',
+      '--fallback-style=Microsoft',
+      '--function-arg-placeholders',
+      '--header-insertion-decorators',
+      '--header-insertion=never',
+      '--limit-results=10',
+      '--pch-storage=memory',
+      '--query-driver=/usr/include/*',
+      '--suggest-missing-includes',
+    },
+  },
   pyright = {
     python = {
       analysis = {
@@ -100,6 +118,7 @@ local servers = {
   },
   lua_ls = {
     Lua = {
+      completion = { callSnippet = 'Both' },
       diagnostics = {
         globals = { 'vim', 'jit' },
         neededFileStatus = true,
