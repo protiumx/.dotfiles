382f587180f6c296802f84adf4e2fe9b534c0aed
Brian Mayo <ioprotium@gmail.com>
2024-09-23 21:41:06 +0000

    testing flash.nvim

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -28,7 +28,7 @@ local lazy_config = {
     -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
     border = 'none',
     -- The backdrop opacity. 0 is fully opaque, 100 is fully transparent.
-    backdrop = 60,
+    backdrop = 100,
     title = nil, ---@type string only works when border is not "none"
     title_pos = 'center', ---@type "center" | "left" | "right"
     -- Show pills on top of the Lazy window
---
+++ stow/nvim/.config/nvim/lua/plugins/flash/init.lua
@@ -0,0 +1,67 @@
+return {
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  opts = {
+    search = {
+      multi_window = false,
+      forward = true,
+      wrap = true,
+      mode = 'exact',
+      -- behave like `incsearch`
+      incremental = false,
+      exclude = {
+        'notify',
+        'cmp_menu',
+        'noice',
+        'flash_prompt',
+        function(win)
+          -- exclude non-focusable windows
+          return not vim.api.nvim_win_get_config(win).focusable
+        end,
+      },
+      max_length = false, ---@type number|false
+    },
+    jump = {
+      -- save location in the jumplist
+      jumplist = false,
+      -- jump position
+      pos = 'start', ---@type "start" | "end" | "range"
+      -- add pattern to search history
+      history = false,
+      -- add pattern to search register
+      register = false,
+      -- clear highlight after jump
+      nohlsearch = false,
+      -- automatically jump when there is only one match
+      autojump = true,
+    },
+    modes = {
+      char = {
+        enabled = false,
+      },
+    },
+    prompt = {
+      enabled = false,
+    },
+  },
+  keys = {
+    {
+      's',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump()
+      end,
+      desc = 'Flash',
+    },
+    {
+      'S',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump({
+          search = { multi_window = true },
+        })
+      end,
+      desc = 'Flash Treesitter',
+    },
+  },
+}
