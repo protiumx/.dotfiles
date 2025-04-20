88524621766b051af101d30a4cd9eacfc8958a03
Brian Mayo <ioprotium@gmail.com>
2025-04-20 18:55:26 +0000

    update build cmd for fzf native

--- stow/nvim/.config/nvim/lua/plugins/telescope/init.lua
+++ stow/nvim/.config/nvim/lua/plugins/telescope/init.lua
@@ -8,7 +8,7 @@ return {
     'nvim-telescope/telescope-file-browser.nvim',
     {
       'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
     },
   },
   config = function()
