22edaaf7b6da0e39a4399d0bf582d30c6105ef1d
Brian Mayo <ioprotium@gmail.com>
2025-04-20 19:02:05 +0000

    remove unused settings

--- stow/nvim/.config/nvim/lua/plugins/conform/init.lua
+++ stow/nvim/.config/nvim/lua/plugins/conform/init.lua
@@ -3,20 +3,4 @@ return {
   config = function()
     require('plugins.conform.config')()
   end,
-  opts = {
-    format_on_save = {
-      timeout_ms = 1000,
-      lsp_fallback = true,
-    },
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      python = { 'black' },
-      ocaml = { 'ocamlformat' },
-      javascript = { 'prettier' },
-      json = { 'prettier' },
-      html = { 'prettier' },
-      markdown = { 'prettier' },
-      go = { 'gofumpt', 'goimports' }, -- runs both
-    },
-  },
 }
