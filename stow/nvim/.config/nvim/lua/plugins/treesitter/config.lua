36826a2a8d3dc145517a8a902d17af60bdc0c20a
Brian Mayo <ioprotium@gmail.com>
2025-07-18 23:55:29 +0200

    nvim/treesitter: do not autoinstall parsers

--- stow/nvim/.config/nvim/lua/plugins/treesitter/config.lua
+++ stow/nvim/.config/nvim/lua/plugins/treesitter/config.lua
@@ -112,7 +112,7 @@ return function()
       'yaml',
     },
     sync_install = true,
-    auto_install = true,
+    auto_install = false,
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = false,
