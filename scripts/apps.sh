700cff04b5ff955e1ab48529a97b3c872f4dd154
Brian Mayo <ioprotium@gmail.com>
2025-07-19 00:08:45 +0200

    scripts: remove mas apps

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -11,15 +11,3 @@ install_macos_apps() {
   info "Installing macOS apps..."
   install_brew_casks "${apps[@]}"
 }
-
-install_masApps() {
-  local apps=(
-    "1444383602" # Good Notes 5
-    "768053424"  # Gappling: svg viewer https://apps.apple.com/us/app/gapplin/id768053424
-  )
-
-  info "Installing App Store apps..."
-  for app in "${apps[@]}"; do
-    mas install "$app"
-  done
-}
