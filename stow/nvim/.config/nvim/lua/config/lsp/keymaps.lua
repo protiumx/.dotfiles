e7c7e3d7af294254a425f8a8a4ba390d88d20e01
Brian Mayo <ioprotium@gmail.com>
2025-07-21 01:26:51 +0200

    nvim/lsp: mapping for telescope document symbols

--- stow/nvim/.config/nvim/lua/config/lsp/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/lsp/keymaps.lua
@@ -7,7 +7,6 @@ function M.setup(bufnr)
 
   nmap('gd', vim.lsp.buf.definition, 'Go to definition')
   nmap('<C-l>d', ':vsplit | lua vim.lsp.buf.definition()<CR>', 'Go to definition vertical split')
-  nmap('gt', vim.lsp.buf.type_definition, 'Show type definition')
   nmap(
     '<C-l>i',
     ':vsplit | lua vim.lsp.buf.implementation()<CR>',
@@ -31,9 +30,8 @@ function M.setup(bufnr)
   nmap('<C-l>y', function()
     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
   end, 'Toggle inlay hints')
-
+  nmap('<C-l>o', '<cmd>Telescope lsp_document_symbols', 'Show signature help')
   nmap('gh', '<cmd>Telescope lsp_references<CR>', 'Show signature help')
-  nmap('<Leader>ca', vim.lsp.buf.code_action, 'Code Action')
   nmap('gT', vim.lsp.buf.type_definition, 'Type definition')
 
   -- Comment utils - uses LSP
