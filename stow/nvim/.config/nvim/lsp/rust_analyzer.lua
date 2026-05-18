7b62b5401a26b7fe44b3ee379d41bb3ec1287ad9
Brian Mayo <ioprotium@gmail.com>
2026-05-19 00:20:56 +0200

    nvim/lsp/rust: update config

--- stow/nvim/.config/nvim/lsp/rust_analyzer.lua
+++ stow/nvim/.config/nvim/lsp/rust_analyzer.lua
@@ -40,9 +40,10 @@ return {
 
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
-    local reuse_active = is_library(fname)
-    if reuse_active then
-      return reuse_active
+    local reused_dir = is_library(fname)
+    if reused_dir then
+      on_dir(reused_dir)
+      return
     end
 
     local cargo_crate_dir = util.root_pattern('Cargo.toml')(fname)
@@ -91,6 +92,13 @@ return {
   capabilities = {
     experimental = {
       serverStatusNotification = true,
+      commands = {
+        commands = {
+          'rust-analyzer.showReferences',
+          'rust-analyzer.runSingle',
+          'rust-analyzer.debugSingle',
+        },
+      },
     },
   },
 
@@ -98,6 +106,25 @@ return {
     -- See https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26
     if config.settings and config.settings['rust-analyzer'] then
       init_params.initializationOptions = config.settings['rust-analyzer']
+    end
+
+    ---@param command table{ title: string, command: string, arguments: any[] }
+    vim.lsp.commands['rust-analyzer.runSingle'] = function(command)
+      local r = command.arguments[1]
+      local cmd = { 'cargo', unpack(r.args.cargoArgs) }
+      if r.args.executableArgs and #r.args.executableArgs > 0 then
+        vim.list_extend(cmd, { '--', unpack(r.args.executableArgs) })
+      end
+
+      local proc = vim.system(cmd, { cwd = r.args.cwd, env = r.args.environment })
+
+      local result = proc:wait()
+
+      if result.code == 0 then
+        vim.notify(result.stdout, vim.log.levels.INFO)
+      else
+        vim.notify(result.stderr, vim.log.levels.ERROR)
+      end
     end
   end,
 
@@ -124,8 +151,19 @@ return {
         command = 'clippy',
         extraArgs = { '--no-deps' },
       },
-      inlay_hints = {
-        auto = false,
+      inlay_hints = {},
+      lens = {
+        debug = { enable = true },
+        enable = true,
+        implementations = { enable = true },
+        references = {
+          adt = { enable = true },
+          enumVariant = { enable = true },
+          method = { enable = true },
+          trait = { enable = true },
+        },
+        run = { enable = true },
+        updateTest = { enable = true },
       },
       procMacro = {
         enable = true,
